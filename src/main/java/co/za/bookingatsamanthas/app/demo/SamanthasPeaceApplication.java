c9f90498a29dcfd1fa5455f6dfca56f7b50eccc7
prothedev <tebogoalbanlefatola@gmail.com>
2023-01-26 16:00:51 +0200

    init

---
+++ src/main/java/co/za/bookingatsamanthas/app/demo/SamanthasPeaceApplication.java
@@ -0,0 +1,13 @@
+package co.za.bookingatsamanthas.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamanthasPeaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamanthasPeaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/za/bookingatsamanthas/app/demo/SamanthasPeaceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.bookingatsamanthas.app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamanthasPeaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
