35fb5703cc6970dbb60b66adea6cc4ed29449fb5
prothedev <tebogoalbanlefatola@gmail.com>
2023-02-07 12:28:23 +0200

    // Reconfigured the Booking Repesitory

--- src/main/java/co/za/bookingatsamanthas/app/demo/Repository/Booking.java
+++ src/main/java/co/za/bookingatsamanthas/app/demo/Repository/Booking.java
@@ -18,41 +18,12 @@ public class Booking {
     @Id
     private ObjectId id;
     private String bookedBy;
-    private RoomNumber roomNumber;
+//    private RoomNumber roomNumber;
     private String bookingDate;
     private String dayOfVisit;
     private String dayOfDeparture;
     private String visitSpecification;
     private String amount;
 
-
-    public Booking madeBy(String email){
-        this.bookedBy = email;
-        return this;
-    }
-
-    public Booking atRoomNumber(RoomNumber roomNumber){
-        this.roomNumber = roomNumber;
-        return this;
-    }
-
-    public Booking from(String dayofVisit){
-        this.dayOfVisit = dayofVisit;
-        return this;
-    }
-
-    public Booking to(String dayOfDeparture){
-        this.dayOfDeparture = dayOfDeparture;
-        return this;
-    }
-
-    public Booking specifyRequests(String request){
-        this.visitSpecification = request;
-        return this;
-    }
-
-    public Booking ConfirmBooking(){
-        return this;
-    }
 
 }
